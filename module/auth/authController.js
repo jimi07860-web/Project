c81081e189a63179f607ecd9515d120e1b655262
jimi07860-web <jimi07860@gmail.com>
2026-04-05 20:22:11 +0530

    Update authController.js

--- module/auth/authController.js
+++ module/auth/authController.js
@@ -16,7 +16,7 @@ export const signupController= asyncHandler(async (req, res) => {
 
 export const loginController= asyncHandler( async (req, res) => {
     const payload= authSchema.parse(req.body)
-    //console.log("payload", payload);
+    console.log("payload", payload);
     if(!payload)
         throw new Error("invalid credential")
     const {user, token}= await loginService(payload)
